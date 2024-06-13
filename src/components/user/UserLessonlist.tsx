65f7edb9399a0e095e1d6442d04df78cd99e6f1d
hjjj99 <hinngreen@naver.com>
2024-06-13 15:22:54 +0900

    ✨Feat: 마이페이지 구현 및 컴포넌트 분리

--- src/components/user/UserLessonlist.tsx
+++ src/components/user/UserLessonlist.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import LessonlistCard from '../../../components/user/LessonlistCard';
+import LessonlistCard from './LessonlistCard';
+import NavbarUser from '../common/NavbarUser';
+import UserMypageMenu from './UserMypageMenu';
 
 const UserLessonlist = () => {
     return (
