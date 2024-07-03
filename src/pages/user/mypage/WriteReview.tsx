19bc22fd8b6c69cb718d0d2a86baf666d67a47b3
hjjj99 <hinngreen@naver.com>
2024-07-03 23:31:23 +0900

    ✨Feat: 유저 리뷰 작성 페이지 제작

--- src/pages/user/mypage/WriteReview.tsx
+++ src/pages/user/mypage/WriteReview.tsx
@@ -1,17 +1,183 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import NavbarUser from "../../../components/common/NavbarUser";
+import NavbarUserMobile from "../../../components/common/NavbarUserMobile";
 
-const WriteReview = () => {
+interface Lesson {
+    lessonId: number;
+    teamId: number;
+    teamName: string;
+    resortName: string;
+    instructorId: number;
+    instructorName: string;
+    profileUrl: string;
+    lessonDate: string;
+    lessonStatus: string;
+    startTime: string;
+    duration: number;
+    hasReview: boolean;
+    studentCount: number;
+}
+
+interface Tag {
+    tagReviewId: number;
+    tagName: string;
+}
+
+const tags: Tag[] = [
+    { tagReviewId: 1, tagName: "친절해요" },
+    { tagReviewId: 2, tagName: "재미있어요" },
+    { tagReviewId: 3, tagName: "열정적이에요" },
+    { tagReviewId: 4, tagName: "학생 친화적" },
+    { tagReviewId: 5, tagName: "전문 지식" },
+    { tagReviewId: 6, tagName: "불친절해요" },
+    { tagReviewId: 7, tagName: "무서워요" },
+    { tagReviewId: 8, tagName: "대충해요" },
+    { tagReviewId: 9, tagName: "이해하기 어려워요" },
+    { tagReviewId: 10, tagName: "준비 부족" },
+];
+
+const WriteReview: React.FC = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const { lesson } = (location.state as { lesson: Lesson }) || {};
 
+    const [rating, setRating] = useState<number>(0);
+    const [hoverRating, setHoverRating] = useState<number>(0);
+    const [selectedTags, setSelectedTags] = useState<number[]>([]);
+    const [reviewContent, setReviewContent] = useState<string>("");
+
+    const formatDate = (dateString: string) => {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const day = date.getDate().toString().padStart(2, "0");
+        return `${year}.${month}.${day}`;
+    };
+
+    const handleTagClick = (id: number) => {
+        setSelectedTags((prevTags) =>
+            prevTags.includes(id)
+                ? prevTags.filter((tag) => tag !== id)
+                : [...prevTags, id]
+        );
+    };
+
+    const handleSubmit = () => {
+        const reviewData = {
+            lessonId: lesson.lessonId,
+            rating,
+            content: reviewContent,
+            reviewTags: selectedTags,
+        };
+        console.log(reviewData);
+        // api 요청
+        navigate(-1);
+    };
 
     return (
-        <div className='flex flex-col w-full h-screen pl-12 items-start'>
-            <div className='pt-12 pb-12 font-extrabold text-black text-2xl'>리뷰 작성</div>
-            <div className='flex flex-col bg-primary-50 w-4/5 h-4/6 rounded-lg shadow-md items-center py-12 space-y-10'>
-                <div className='flex flex-row bg-white w-4/5 h-2/6 rounded-lg'></div>
-                <div className='flex flex-row bg-white w-4/5 h-3/6 rounded-lg'></div>
-                <div className='flex items-center justify-center bg-slate-400 w-1/6 h-12 rounded-lg text-white'>작성 완료</div>
+        <div className="flex flex-col w-full h-screen items-center">
+            <div className="w-full">
+                {innerWidth > 640 ? <NavbarUser /> : <NavbarUserMobile />}
+            </div>
+            <div className="w-full px-12 pt-8 pb-12 font-extrabold text-black text-2xl">
+                리뷰 작성
+            </div>
+            <div className="flex flex-col bg-primary-50 w-4/5 rounded-lg shadow-md items-center py-12 space-y-10 ">
+                <div className="flex sm:flex-row flex-col bg-white w-4/5 rounded-lg items-center justify-center p-6">
+                    <img
+                        src={lesson.profileUrl}
+                        alt="Profile"
+                        className="w-24 h-24 rounded-full"
+                    />
+                    <div className="flex flex-col ml-4 justify-center items-center space-y-0.5">
+                        <div className="font-bold text-xl">
+                            {lesson.resortName}
+                        </div>
+                        <div>{lesson.teamName}</div>
+                        <div className="text-gray-500 text-sm">
+                            {formatDate(lesson.lessonDate)} {lesson.startTime}
+                        </div>
+                        <div>{lesson.instructorName}</div>
+                    </div>
+                </div>
+                <div className="flex flex-col bg-white w-4/5 rounded-lg p-6">
+                    <div className="flex flex-col space-y-4">
+                        <div className="flex flex-col items-center space-y-2">
+                            <div className="font-bold text-lg">
+                                이번 강습 어땠나요?
+                            </div>
+                            <div className="flex flex-row space-x-1">
+                                {[...Array(5)].map((_, index) => {
+                                    const starValue = index + 1;
+                                    return (
+                                        <label key={starValue}>
+                                            <input
+                                                type="radio"
+                                                name="rating"
+                                                value={starValue}
+                                                onClick={() =>
+                                                    setRating(starValue / 2)
+                                                }
+                                                className="hidden"
+                                            />
+                                            <FaStar
+                                                size={24}
+                                                color={
+                                                    hoverRating >= starValue ||
+                                                    rating >= starValue / 2
+                                                        ? "orange"
+                                                        : "gray"
+                                                }
+                                                onMouseEnter={() =>
+                                                    setHoverRating(starValue)
+                                                }
+                                                onMouseLeave={() =>
+                                                    setHoverRating(0)
+                                                }
+                                            />
+                                        </label>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className="grid sm:grid-cols-4 grid-cols-2 sm:gap-4 gap-2 ">
+                            {tags.map((tag) => (
+                                <button
+                                    key={tag.tagReviewId}
+                                    className={`px-2 py-2 sm:w-28 w-20 sm:text-md text-xs h-8 rounded-full truncate ${
+                                        selectedTags.includes(tag.tagReviewId)
+                                            ? "bg-blue-500 text-white"
+                                            : "bg-blue-100"
+                                    }`}
+                                    onClick={() =>
+                                        handleTagClick(tag.tagReviewId)
+                                    }
+                                >
+                                    {tag.tagName}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col bg-white w-4/5 rounded-lg p-6">
+                    <label htmlFor="review" className="font-bold pb-5">
+                        리뷰를 작성해주세요
+                    </label>
+                    <textarea
+                        id="review"
+                        value={reviewContent}
+                        onChange={(e) => setReviewContent(e.target.value)}
+                        className="p-4 bg-gray-100 rounded-lg w-full h-32"
+                    ></textarea>
+                </div>
+                <button
+                    className="flex items-center justify-center bg-blue-500 sm:w-1/6 w-24 h-12 rounded-lg text-white"
+                    onClick={handleSubmit}
+                >
+                    작성 완료
+                </button>
             </div>
         </div>
     );
