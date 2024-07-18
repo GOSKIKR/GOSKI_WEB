ebea2397099be6e9cd11f02b43831a827d29b454
hjjj99 <hinngreen@naver.com>
2024-07-18 21:22:57 +0900

    ✨Feat: 결제 전 강사 지정 페이지 api 연결

--- src/components/user/SelectInstructor.tsx
+++ src/components/user/SelectInstructor.tsx
@@ -1,122 +1,59 @@
 import React, { useState } from "react";
+import { Instructor } from "../../dto/UserInstructorDTO";
 
-interface CertificateInfo {
-    certificateId: number;
-    certificateName: string;
-    certificateType: string;
-    certificateImageUrl: string;
+interface SelectInstructorProps {
+    instructors: Instructor[];
+    onSelect: (instructor: Instructor) => void;
 }
 
-interface Instructor {
-    instructorId: number;
-    userName: string;
-    teamId: number;
-    teamName: string;
-    position: string;
-    description: string;
-    instructorUrl: string;
-    gender: string;
-    certificateInfo: CertificateInfo[];
-    rating: number;
-    reviewCount: number;
-    cost: number;
-    basicFee: number;
-    peopleOptionFee: number;
-    designatedFee: number;
-    levelOptionFee: number;
-    lessonType: string;
-    reviews: any[];
-}
-
-const dummyInstructors = [
-    {
-        instructorId: 1,
-        userName: "고승민",
-        teamId: 1,
-        teamName: "팀A",
-        position: "교육팀장",
-        description: "소개소개소개",
-        instructorUrl: "https://via.placeholder.com/150",
-        gender: "남성",
-        certificateInfo: [
-            {
-                certificateId: 1,
-                certificateName: "Level 1",
-                certificateType: "Ski",
-                certificateImageUrl: "https://via.placeholder.com/50",
-            },
-        ],
-        rating: 4.5,
-        reviewCount: 10,
-        cost: 100000,
-        basicFee: 100000,
-        peopleOptionFee: 10000,
-        designatedFee: 50000,
-        levelOptionFee: 20000,
-        lessonType: "Ski",
-        reviews: [],
-    },
-    {
-        instructorId: 2,
-        userName: "임종률",
-        teamId: 2,
-        teamName: "팀B",
-        position: "교육팀장",
-        description: "소개소개소개",
-        instructorUrl: "https://via.placeholder.com/150",
-        gender: "남성",
-        certificateInfo: [
-            {
-                certificateId: 2,
-                certificateName: "Level 1",
-                certificateType: "Board",
-                certificateImageUrl: "https://via.placeholder.com/50",
-            },
-        ],
-        rating: 4.7,
-        reviewCount: 12,
-        cost: 100000,
-        basicFee: 100000,
-        peopleOptionFee: 10000,
-        designatedFee: 50000,
-        levelOptionFee: 20000,
-        lessonType: "Board",
-        reviews: [],
-    },
-];
-
-const SelectInstructor: React.FC = () => {
+const SelectInstructor: React.FC<SelectInstructorProps> = ({
+    instructors,
+    onSelect,
+}) => {
     const [selectedInstructor, setSelectedInstructor] =
         useState<Instructor | null>(null);
-    const [instructors] = useState<Instructor[]>(dummyInstructors);
+    const [expandedDescription, setExpandedDescription] = useState<
+        number | null
+    >(null);
+
+    const toggleDescription = (id: number) => {
+        setExpandedDescription(expandedDescription === id ? null : id);
+    };
+
+    const handleInstructorClick = (instructor: Instructor) => {
+        setSelectedInstructor(instructor);
+        onSelect(instructor);
+    };
 
     return (
-        <div className="w-full sm:h-96 bg-primary-50 rounded-lg shadow-md flex sm:flex-row flex-col items-center sm:justify-center sm:space-x-5 space-y-3 py-5">
+        <div className="w-full sm:h-[500px] bg-primary-50 rounded-lg shadow-md flex sm:flex-row flex-col items-center sm:justify-center sm:space-x-5 sm:space-y-0 space-y-3 py-5">
             <div className="flex flex-col bg-white sm:h-full sm:w-5/12 rounded-lg p-4">
                 <div className="flex flex-col justify-center items-center">
                     <div className="font-extrabold text-xl">강사 목록</div>
-                    <div className="font-thin text-xs text-gray-500 pt-2">
+                    <div className="font-thin text-xs text-gray-500 pt-0.5">
                         지정 강사 선택 시 추가 요금이 부과됩니다.
                     </div>
                 </div>
-                <div className="space-y-2 pt-2">
+                <div className="space-y-2 mt-4 overflow-auto flex flex-col items-center">
                     {instructors.map((instructor) => (
                         <div
                             key={instructor.instructorId}
-                            className="cursor-pointer p-2 border-b border-gray-200 flex items-center bg-primary-100 rounded-md"
-                            onClick={() => setSelectedInstructor(instructor)}
+                            className="cursor-pointer p-3 flex flex-row space-x-3 w-11/12 shadow-md justify-between items-center bg-primary-100 rounded-md"
+                            onClick={() => handleInstructorClick(instructor)}
                         >
-                            <img
-                                src={instructor.instructorUrl}
-                                alt={instructor.userName}
-                                className="w-12 h-12 rounded-full mr-4"
-                            />
-                            <div className="flex flex-col space-y-0.5">
+                            <div className="w-1/4 flex justify-center">
+                                <img
+                                    src={instructor.instructorUrl}
+                                    alt={instructor.userName}
+                                    className="w-12 h-12 rounded-full"
+                                />
+                            </div>
+                            <div className="flex flex-col space-y-1 w-3/4">
                                 <div className="flex flex-row space-x-3">
                                     <div className="text-xs font-extrabold">
                                         직책
                                     </div>
-                                    <div className="text-xs bg-primary-400 text-white px-0.5 rounded-sm">
+                                    <div className="text-xs bg-primary-400 w-5 text-center text-white px-0.5 rounded-md">
                                         {instructor.position}
                                     </div>
                                 </div>
@@ -128,82 +65,132 @@ const SelectInstructor: React.FC = () => {
                                         {instructor.userName}
                                     </div>
                                 </div>
-                                <div className="text-xs text-gray-500 px-0.5 rounded-sm">
-                                    {instructor.description}
+                                <div className="text-[10px] text-gray-500 px-0.5 rounded-sm">
+                                    {expandedDescription ===
+                                    instructor.instructorId ? (
+                                        <>
+                                            {instructor.description}{" "}
+                                            <span
+                                                className="text-primary-500 cursor-pointer"
+                                                onClick={() =>
+                                                    toggleDescription(
+                                                        instructor.instructorId
+                                                    )
+                                                }
+                                            >
+                                                (접기)
+                                            </span>
+                                        </>
+                                    ) : (
+                                        <>
+                                            {instructor.description.length > 50
+                                                ? instructor.description.substring(
+                                                      0,
+                                                      50
+                                                  ) + "..."
+                                                : instructor.description}{" "}
+                                            {instructor.description.length >
+                                                50 && (
+                                                <span
+                                                    className="text-primary-500 cursor-pointer"
+                                                    onClick={() =>
+                                                        toggleDescription(
+                                                            instructor.instructorId
+                                                        )
+                                                    }
+                                                >
+                                                    (더보기)
+                                                </span>
+                                            )}
+                                        </>
+                                    )}
                                 </div>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
-            <div className=" w-10/12 bg-white sm:h-full sm:w-5/12 rounded-lg p-4">
+            <div className="w-10/12 bg-white sm:h-full sm:w-5/12 rounded-lg p-4">
                 {selectedInstructor ? (
-                    <>
+                    <div className="flex flex-col relative h-full">
                         <div className="font-extrabold text-xl text-center">
                             강사 프로필
                         </div>
-                        <div className="flex items-center justify-center mt-4">
+                        <div className="flex items-center justify-center w-full mt-4">
                             <img
                                 src={selectedInstructor.instructorUrl}
                                 alt={selectedInstructor.userName}
                                 className="w-24 h-24 rounded-full"
                             />
                         </div>
-                        <div className="flex flex-col space-y-1.5 pt-5 pb-3">
-                            <div className="flex flex-row space-x-6">
-                                <div className="text-xs font-extrabold">
+                        <div className="flex flex-col space-y-3 pt-5 pb-3">
+                            <div className="flex flex-row items-center space-x-2">
+                                <div className="text-xs font-extrabold w-20">
                                     직책
                                 </div>
-                                <div className="text-xs bg-primary-400 text-white px-0.5 rounded-sm">
-                                    {selectedInstructor.position}
+                                <div className="text-xs text-white px-2 flex-1">
+                                    <div className="bg-primary-400 w-5 text-center rounded-md">
+                                        {selectedInstructor.position}
+                                    </div>
                                 </div>
                             </div>
-                            <div className="flex flex-row space-x-6">
-                                <div className="text-xs font-extrabold">
+                            <div className="flex flex-row items-center space-x-2">
+                                <div className="text-xs font-extrabold w-20">
                                     이름
                                 </div>
-                                <div className="text-xs text-black px-0.5 rounded-sm">
+                                <div className="text-xs text-black px-2 rounded-sm flex-1">
                                     {selectedInstructor.userName}
                                 </div>
                             </div>
-                            <div className="flex flex-row space-x-6">
-                                <div className="text-xs font-extrabold">
+                            <div className="flex flex-row items-center space-x-2">
+                                <div className="text-xs font-extrabold w-20">
                                     성별
                                 </div>
-                                <div className="text-xs text-black px-0.5 rounded-sm">
+                                <div className="text-xs text-black px-2 rounded-sm flex-1">
                                     {selectedInstructor.gender}
                                 </div>
                             </div>
-                            <div className="flex flex-row space-x-4">
-                                <div className="text-xs font-extrabold">
+                            <div className="flex flex-row items-center space-x-2">
+                                <div className="text-xs font-extrabold w-20">
                                     자격명
                                 </div>
-                                <div className="text-xs text-black px-0.5 rounded-sm">
-                                    {selectedInstructor.certificateInfo.map(
-                                        (cert) => (
-                                            <div key={cert.certificateId}>
-                                                {cert.certificateName} (
-                                                {cert.certificateType})
-                                            </div>
+                                <div className="text-xs text-black px-2 rounded-sm flex-1">
+                                    {selectedInstructor.certificateInfo &&
+                                    selectedInstructor.certificateInfo.length >
+                                        0 ? (
+                                        selectedInstructor.certificateInfo.map(
+                                            (cert) => (
+                                                <div key={cert.certificateId}>
+                                                    {cert.certificateName} (
+                                                    {cert.certificateType})
+                                                </div>
+                                            )
                                         )
+                                    ) : (
+                                        <div>자격 정보 없음</div>
                                     )}
                                 </div>
                             </div>
-                            <div className="flex flex-row space-x-2">
-                                <div className="text-xs font-extrabold">
+                            <div className="flex flex-row items-center space-x-2">
+                                <div className="text-xs font-extrabold w-20">
                                     자기소개
                                 </div>
-                                <div className="text-xs text-black px-0.5 rounded-sm">
+                                <div className="text-xs text-black px-2 rounded-sm flex-1">
                                     {selectedInstructor.description}
                                 </div>
                             </div>
                         </div>
-                        <div className="flex justify-center">
-                            <div className="bg-primary-500 text-white font-extrabold text-lg w-12 text-center items-center justify-center rounded-lg">
+                        <div className="flex justify-center absolute bottom-0.5 items-center w-full">
+                            <div
+                                onClick={() =>
+                                    handleInstructorClick(selectedInstructor)
+                                }
+                                className="bg-primary-500 text-white font-extrabold text-lg w-20 py-1 text-center items-center justify-center rounded-lg cursor-pointer"
+                            >
                                 선택
                             </div>
                         </div>
-                    </>
+                    </div>
                 ) : (
                     <div className="flex justify-center items-center h-full text-gray-500">
                         강사를 선택해 주세요
