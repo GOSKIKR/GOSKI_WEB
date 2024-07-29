f9239bf64ef6fa40c05f8c0578029ba016390aca
KimTaeHun <kth32105@naver.com>
2024-07-29 13:04:20 +0900

    :fix: Fix:FCM 주석 처리

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,21 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+// http 이슈로 인해 서비스 워커 등록 주석 처리
 // 서비스 워커 등록
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
-    .then((registration) => {
-      console.log(
-        "Service Worker registration successful with scope: ",
-        registration.scope
-      );
-    })
-    .catch((error) => {
-      console.log("Service Worker registration failed: ", error);
-    });
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("/firebase-messaging-sw.js")
+//     .then((registration) => {
+//       console.log(
+//         "Service Worker registration successful with scope: ",
+//         registration.scope
+//       );
+//     })
+//     .catch((error) => {
+//       console.log("Service Worker registration failed: ", error);
+//     });
+// }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
