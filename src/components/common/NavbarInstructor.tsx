8a77e3f4cd4d5f716d92e2f5809476eb7c10715c
Seung Ho Jang <jason9865@naver.com>
2024-08-05 14:10:04 +0900

    :zap: Update : 헤더 - 팀 관리 버튼 클릭 시 role 별로 이동하는 페이지 다르게 수정

--- src/components/common/NavbarInstructor.tsx
+++ src/components/common/NavbarInstructor.tsx
@@ -117,6 +117,10 @@ const NavbarInstructor = () => {
     }
   };
 
+  const handleNavigate = () => {
+    role === "OWNER" ? navigate("/instructor/team/regist") : navigate("/instructor/team/edit")
+  }
+
   return (
     <div className="flex flex-row h-12 w-full justify-between items-center bg-primary-200 box-border px-4 z-50">
       <div
@@ -128,7 +132,7 @@ const NavbarInstructor = () => {
       <div className="flex flex-row flex-1 justify-normal">
         <div
           className="text-black sm:text-xl text-sm p-5 cursor-pointer"
-          onClick={() => navigate("/instructor/team/regist")}
+          onClick={handleNavigate}
         >
           팀 관리
         </div>
